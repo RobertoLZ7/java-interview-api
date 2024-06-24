1fc06d9c577e150b26da90f9b829bff05e43dd83
roberto.litzon <roberto@relativity6.com>
2024-06-24 11:46:00 -0600

    remove unused import

--- src/main/java/com/talentreef/interviewquestions/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/talentreef/interviewquestions/RestResponseEntityExceptionHandler.java
@@ -14,7 +14,6 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.List;
-import java.util.Objects;
 
 @RestControllerAdvice
 @Slf4j
