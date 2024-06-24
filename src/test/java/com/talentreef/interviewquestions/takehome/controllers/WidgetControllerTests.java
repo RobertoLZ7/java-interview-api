c78f7f85c07b813121ac42288d41ee308930f2b6
roberto.litzon <roberto@relativity6.com>
2024-06-24 13:26:31 -0600

    added coverage

--- src/test/java/com/talentreef/interviewquestions/takehome/controllers/WidgetControllerTests.java
+++ src/test/java/com/talentreef/interviewquestions/takehome/controllers/WidgetControllerTests.java
@@ -3,14 +3,13 @@ package com.talentreef.interviewquestions.takehome.controllers;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.talentreef.interviewquestions.takehome.models.Widget;
-import com.talentreef.interviewquestions.takehome.respositories.WidgetRepository;
 import com.talentreef.interviewquestions.takehome.services.WidgetService;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,10 +18,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -60,4 +61,177 @@ public class WidgetControllerTests {
     assertThat(parsedResult).isEqualTo(allWidgets);
   }
 
+  @Test
+  public void when_getWidgetByName_expect_widget() throws Exception {
+    Widget firstWidget = Widget.builder().name("test widget").build();
+    Widget secondWidget = Widget.builder().name("Widget von Hammersmark").build();
+
+    when(widgetService.getWidgetByName(any())).thenReturn(firstWidget);
+
+    MvcResult result = mockMvc.perform(get("/v1/widgets?name=test widget"))
+            .andExpect(status().isOk())
+            .andDo(print())
+            .andReturn();
+
+    Widget parsedResult = objectMapper.readValue(result.getResponse().getContentAsString(),
+            new TypeReference<Widget>() {});
+    assertThat(parsedResult).isEqualTo(firstWidget);
+  }
+
+  @Test
+  public void when_createWidget_expect_widget() throws Exception {
+    Widget newWidget = Widget.builder().name("new widget").price(new BigDecimal("20.0")).description("this is a description example").build();
+    String jsonRequest = objectMapper.writeValueAsString(newWidget);
+    when(widgetService.createWidget(any())).thenReturn(newWidget);
+
+    MvcResult result = mockMvc.perform(post("/v1/widgets").contentType(MediaType.APPLICATION_JSON)
+            .content(jsonRequest))
+            .andExpect(status().isOk())
+            .andDo(print())
+            .andReturn();
+
+    Widget parsedResult = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<Widget>() {});
+    assertThat(parsedResult).isEqualTo(newWidget);
+  }
+  @Test
+  public void when_createWidget_withoutPrice_expect_badRequest() throws Exception {
+    Widget newWidget = Widget.builder().name("new widget").description("this is a description example").build();
+    String jsonRequest = objectMapper.writeValueAsString(newWidget);
+
+    MvcResult result = mockMvc.perform(post("/v1/widgets").contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isBadRequest())
+            .andDo(print())
+            .andReturn();
+
+    assertThat(result.getResponse().getErrorMessage()).isEqualTo("Invalid request content.");
+  }
+
+  @Test
+  public void when_createWidget_withoutDescription_expect_badRequest() throws Exception {
+    Widget newWidget = Widget.builder().name("new widget").price(new BigDecimal("20.0")).build();
+    String jsonRequest = objectMapper.writeValueAsString(newWidget);
+
+    MvcResult result = mockMvc.perform(post("/v1/widgets").contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isBadRequest())
+            .andDo(print())
+            .andReturn();
+
+    assertThat(result.getResponse().getErrorMessage()).isEqualTo("Invalid request content.");
+  }
+
+  @Test
+  public void when_createWidget_withoutName_expect_badRequest() throws Exception {
+    Widget newWidget = Widget.builder().description("this is a description example").price(new BigDecimal("20.0")).build();
+    String jsonRequest = objectMapper.writeValueAsString(newWidget);
+
+    MvcResult result = mockMvc.perform(post("/v1/widgets").contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isBadRequest())
+            .andDo(print())
+            .andReturn();
+
+    assertThat(result.getResponse().getErrorMessage()).isEqualTo("Invalid request content.");
+  }
+
+  @Test
+  public void when_createWidget_smallerName_expect_badRequest() throws Exception {
+    Widget newWidget = Widget.builder().name("tt").price(new BigDecimal("20.0")).description("this is a description example").build();
+    String jsonRequest = objectMapper.writeValueAsString(newWidget);
+
+    MvcResult result = mockMvc.perform(post("/v1/widgets").contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isBadRequest())
+            .andDo(print())
+            .andReturn();
+
+    assertThat(result.getResponse().getErrorMessage()).isEqualTo("Invalid request content.");
+  }
+
+  @Test
+  public void when_updateWidget_expect_widget() throws Exception {
+    Widget updatedWidget = Widget.builder().name("updated").price(new BigDecimal("20.0")).description("this is a description example").build();
+    String jsonRequest = objectMapper.writeValueAsString(updatedWidget);
+    when(widgetService.updateWidget(any())).thenReturn(updatedWidget);
+
+    MvcResult result = mockMvc.perform(put("/v1/widgets").contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isOk())
+            .andDo(print())
+            .andReturn();
+
+    Widget parsedResult = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<Widget>() {});
+    assertThat(parsedResult).isEqualTo(updatedWidget);
+  }
+
+  @Test
+  public void when_updateWidget_smallDescription_expect_badRequest() throws Exception {
+    Widget updatedWidget = Widget.builder().name("updated").price(new BigDecimal("20.0")).description("desc").build();
+    String jsonRequest = objectMapper.writeValueAsString(updatedWidget);
+
+    MvcResult result = mockMvc.perform(put("/v1/widgets").contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isBadRequest())
+            .andDo(print())
+            .andReturn();
+
+    assertThat(result.getResponse().getErrorMessage()).isEqualTo("Invalid request content.");
+  }
+
+  @Test
+  public void when_updateWidget_higherPrice_expect_badRequest() throws Exception {
+    Widget updatedWidget = Widget.builder().name("updated").price(new BigDecimal("20000.01")).description("this is an example description").build();
+    String jsonRequest = objectMapper.writeValueAsString(updatedWidget);
+
+    MvcResult result = mockMvc.perform(put("/v1/widgets").contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isBadRequest())
+            .andDo(print())
+            .andReturn();
+
+    assertThat(result.getResponse().getErrorMessage()).isEqualTo("Invalid request content.");
+  }
+
+  @Test
+  public void when_updateWidget_lowerPrice_expect_badRequest() throws Exception {
+    Widget updatedWidget = Widget.builder().name("updated").price(new BigDecimal("0.50")).description("this is an example description").build();
+    String jsonRequest = objectMapper.writeValueAsString(updatedWidget);
+
+    MvcResult result = mockMvc.perform(put("/v1/widgets").contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isBadRequest())
+            .andDo(print())
+            .andReturn();
+
+    assertThat(result.getResponse().getErrorMessage()).isEqualTo("Invalid request content.");
+  }
+
+  @Test
+  public void when_updateWidget_exceedDecimals_expect_badRequest() throws Exception {
+    Widget updatedWidget = Widget.builder().name("updated").price(new BigDecimal("20.034")).description("this is an example description").build();
+    String jsonRequest = objectMapper.writeValueAsString(updatedWidget);
+
+    MvcResult result = mockMvc.perform(put("/v1/widgets").contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonRequest))
+            .andExpect(status().isBadRequest())
+            .andDo(print())
+            .andReturn();
+
+    assertThat(result.getResponse().getErrorMessage()).isEqualTo("Invalid request content.");
+  }
+
+  @Test
+  public void when_deleteWidgetByName_expect_widget() throws Exception {
+    Widget deletedWidget = Widget.builder().name("deleted").build();
+    when(widgetService.deleteWidgetByName(any())).thenReturn(deletedWidget);
+
+    MvcResult result = mockMvc.perform(delete("/v1/widgets?name=deleted"))
+            .andExpect(status().isOk())
+            .andDo(print())
+            .andReturn();
+
+    Widget parsedResult = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<Widget>() {});
+    assertThat(parsedResult).isEqualTo(deletedWidget);
+  }
 }
--- src/test/java/com/talentreef/interviewquestions/takehome/services/WidgetServiceTests.java
+++ src/test/java/com/talentreef/interviewquestions/takehome/services/WidgetServiceTests.java
@@ -1,8 +1,12 @@
 package com.talentreef.interviewquestions.takehome.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import com.talentreef.interviewquestions.exceptions.ElementAlreadyExistsException;
+import com.talentreef.interviewquestions.exceptions.ElementNotFoundException;
 import com.talentreef.interviewquestions.takehome.models.Widget;
 import com.talentreef.interviewquestions.takehome.respositories.WidgetRepository;
 import org.junit.Test;
@@ -11,7 +15,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 public class WidgetServiceTests {
@@ -33,4 +39,85 @@ public class WidgetServiceTests {
     assertThat(result).isEqualTo(response);
   }
 
+  @Test
+  public void when_getWidgetByName_expect_findResult() throws Exception {
+    Widget widget = Widget.builder().name("Widgette Nielson").build();
+    when(widgetRepository.findByNameIgnoreCase(any())).thenReturn(Optional.of(widget));
+
+    Widget result = widgetService.getWidgetByName(widget.getName());
+
+    assertThat(result).isEqualTo(widget);
+  }
+
+  @Test
+  public void when_getWidgetByName_expect_notFound() throws Exception {
+    Widget widget = Widget.builder().name("Widgette Nielson").build();
+    when(widgetRepository.findByNameIgnoreCase(any())).thenReturn(Optional.empty());
+
+    assertThatThrownBy(() -> widgetService.getWidgetByName(widget.getName()))
+            .isInstanceOf(ElementNotFoundException.class)
+            .hasMessage("Widget with name=Widgette Nielson not found");
+  }
+
+  @Test
+  public void when_createWidget_expect_widget() throws Exception {
+    Widget widget = Widget.builder().name("Widgette Nielson").build();
+    when(widgetRepository.findByNameIgnoreCase(any())).thenReturn(Optional.empty());
+    when(widgetRepository.save(any())).thenReturn(widget);
+    Widget result = widgetService.createWidget(widget);
+
+    assertThat(result).isEqualTo(widget);
+  }
+
+  @Test
+  public void when_createWidget_expect_alreadyExists() throws Exception {
+    Widget widget = Widget.builder().name("Widgette Nielson").build();
+    when(widgetRepository.findByNameIgnoreCase(any())).thenReturn(Optional.of(widget));
+
+    assertThatThrownBy(() -> widgetService.createWidget(widget))
+            .isInstanceOf(ElementAlreadyExistsException.class)
+            .hasMessage("Widget with name=Widgette Nielson already exists");
+  }
+
+  @Test
+  public void when_updateWidget_expect_widget() throws Exception {
+    Widget previousWidget = Widget.builder().name("Widgette Nielson").build();
+    Widget updatedWidget = Widget.builder().name("Widgette Nielson").price(new BigDecimal("20.0")).description("updated widget description").build();
+    when(widgetRepository.findByNameIgnoreCase(any())).thenReturn(Optional.of(previousWidget));
+    when(widgetRepository.save(any())).thenReturn(updatedWidget);
+
+    Widget result = widgetService.updateWidget(updatedWidget);
+
+    assertThat(result).isEqualTo(updatedWidget);
+  }
+
+  @Test
+  public void when_updateWidget_expect_notFound() throws Exception {
+    Widget updatedWidget = Widget.builder().name("Widgette Nielson").price(new BigDecimal("20.0")).description("updated widget description").build();
+    when(widgetRepository.findByNameIgnoreCase(any())).thenReturn(Optional.empty());
+
+    assertThatThrownBy(() ->widgetService.updateWidget(updatedWidget))
+            .isInstanceOf(ElementNotFoundException.class)
+            .hasMessage("Widget with name=Widgette Nielson not found");
+  }
+
+  @Test
+  public void when_deleteWidgetByName_expect_widget() throws Exception {
+    Widget widget = Widget.builder().name("Widgette Nielson").price(new BigDecimal("20.0")).description("updated widget description").build();
+    when(widgetRepository.findByNameIgnoreCase(any())).thenReturn(Optional.of(widget));
+
+    Widget deleted = widgetService.deleteWidgetByName(widget.getName());
+
+    assertThat(deleted).isEqualTo(widget);
+  }
+
+  @Test
+  public void when_deleteWidgetByName_expect_notFound() throws Exception {
+    Widget widget = Widget.builder().name("Widgette Nielson").price(new BigDecimal("20.0")).description("updated widget description").build();
+    when(widgetRepository.findByNameIgnoreCase(any())).thenReturn(Optional.empty());
+
+    assertThatThrownBy(() ->widgetService.deleteWidgetByName(widget.getName()))
+            .isInstanceOf(ElementNotFoundException.class)
+            .hasMessage("Widget with name=Widgette Nielson not found");
+  }
 }
